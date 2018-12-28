2eb6f07163648b293256561fbc4a0193101c58f9
Mayank Joshi <mj74447@gmail.com>
2018-12-28 21:15:37 +0530

    Locator

---
+++ ip_locator.py
@@ -0,0 +1,11 @@
+from urllib import request
+import json
+
+try:
+    data = json.load(request.urlopen('http://ipinfo.io/json'))
+
+except Exception as e:
+    print(e)
+else:
+    print('you are near {city},{region},{country}'.format(**data))
+    print('   Lat/Lng: {}E'.format(data['loc'].replace(',','N, ')))    
