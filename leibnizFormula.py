fcad1e2ac52b9362e74ef9f23d810d92890bae6e
Mayank Joshi <mj74447@gmail.com>
2019-01-14 23:58:39 +0530

    Add files via upload

---
+++ leibnizFormula.py
@@ -0,0 +1,18 @@
+def odd():
+    x=1
+    while True:
+            yield x
+            x = x+2
+
+def pi_series():
+    odds = odd()
+    approx=0
+    while True:
+        approx += (4 / next(odds)) 
+        yield approx
+        approx -= (4/ next(odds))
+        yield approx
+
+app_pi = pi_series() 
+for x in range(1000):
+    print(next(app_pi))       
